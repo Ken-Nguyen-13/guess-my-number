36efa836c9363aa115371bb7ba1ab685e3b302b1
Ken-Nguyen-13 <ken.ak.nguyen@gmail.com>
2023-05-30 15:27:15 -0400

    added game logic manipulated css

--- starter/script.js
+++ starter/script.js
@@ -2,38 +2,59 @@
 
 // document.querySelector('.guess').value = Math.ceil(Math.random() * 20);
 let mysteryNum = Math.trunc(Math.random() * 20 + 1);
-document.querySelector('.number').textContent = mysteryNum;
+let score = 20;
+let highscore = 0;
 
+//Reset Button
 document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
   mysteryNum = Math.trunc(Math.random() * 20 + 1);
-  document.querySelector('.number').textContent = mysteryNum;
-  document.querySelector('.message').textContent = `Start guessing`;
-  document.querySelector('.score').textContent = 20;
+  document.querySelector('.message').textContent = `Start Guessing!`;
+  document.querySelector('.score').textContent = score;
   document.querySelector('.guess').value = ``;
+  document.querySelector('body').style.backgroundColor = `#222`;
+  document.querySelector('.number').style.width = `15rem`;
+  document.querySelector('.number').textContent = `?`;
+  document.querySelector('h1').textContent = `Guess My Number!`;
 });
 
+//Checking the Number
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  if (document.querySelector('.message').textContent !== `✅ Correct Number!`) {
-    if (guess !== mysteryNum) {
-      document.querySelector('.score').textContent =
-        Number(document.querySelector('.score').textContent) - 1;
-    }
-    if (!guess) {
-      document.querySelector('.message').textContent = `⛔ No Number!`;
+  //Code for message and score
+  if (
+    document.querySelector('.message').textContent !== `✅ Correct Number!` &&
+    document.querySelector('.message').textContent !== `You Lost.`
+  ) {
+    if (!guess || 1 > guess || guess > 20) {
+      //invalid Number
+      document.querySelector(`.message`).textContent = `⛔ Invalid Number!`
     } else if (guess === mysteryNum) {
+      //Correct Number
       document.querySelector('.message').textContent = `✅ Correct Number!`;
-      if (
-        Number(document.querySelector('.score').textContent) >
-        Number(document.querySelector('.highscore').textContent)
-      ) {
-        document.querySelector('.highscore').textContent =
-          document.querySelector('.score').textContent;
+      document.querySelector('body').style.backgroundColor = `#46c63b`;
+      document.querySelector('#game-title').textContent = `You Guessed My Number!`;
+      document.querySelector('.number').style.width = `30rem`;
+      document.querySelector('.number').textContent = mysteryNum;
+      if (highscore < score) {
+        highscore = score;
+        document.querySelector('.highscore').textContent = highscore;
       }
-    } else if (guess > mysteryNum) {
-      document.querySelector('.message').textContent = `Lower!`;
+    } else if (score > 1) {
+      if (guess > mysteryNum) {
+        //Too High
+        score--;
+        document.querySelector('.message').textContent = `Too High!`;
+        document.querySelector('.score').textContent = score;
+      } else if (guess < mysteryNum) {
+        //Too low
+        score--;
+        document.querySelector('.message').textContent = `Too Low!`;
+        document.querySelector('.score').textContent = score;
+      }
     } else {
-      document.querySelector('.message').textContent = `Higher!`;
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('.message').textContent = `You Lost.`;
     }
   }
 });
